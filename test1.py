aacf23b817f41f06ca3717fe81f47d6c042a4b8c
sarahbouraya <bouraya.sarah.1@gmail.com>
2024-12-21 15:25:48 +0100

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,2 +1,8 @@
 import streamlit as st
-st.write("Hello");
+
+st.title("EMC2 / Cloud Module")
+st.header("App pour test des AI app")
+st.video("https://youtu.be/YfZ0zk5Zzcw")
+
+st.sidebar.image("https://seeklogo.com/images/E/ecole-hassania-des-travaux-publics-ehtp-logo-3D5770F217-seeklogo.com.png")
+st.sidebar.header("Master Cloud Computing")
