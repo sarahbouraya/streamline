540743c63e77f2e65dd9fc1c1b588eec70145af2
sarahbouraya <bouraya.sarah.1@gmail.com>
2024-12-21 15:32:24 +0100

    Update test1.py

--- test1.py
+++ test1.py
@@ -6,3 +6,6 @@ st.video("https://youtu.be/YfZ0zk5Zzcw")
 
 st.sidebar.image("https://seeklogo.com/images/E/ecole-hassania-des-travaux-publics-ehtp-logo-3D5770F217-seeklogo.com.png")
 st.sidebar.header("Master Cloud Computing")
+
+
+st.file_uploader()
